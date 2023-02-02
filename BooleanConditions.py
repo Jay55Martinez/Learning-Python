9b16bbfca7753de5d2562e75409d7facad0d1a2e
Jay55Martinez <martinez.jay@northeastern.edu>
2023-02-02 16:52:00 -0500

    boolean condition

---
+++ BooleanConditions.py
@@ -0,0 +1,14 @@
+# basic logic
+x = 2
+print(x == 2) # True
+print(x == 3) # False
+print(x >= 1) # True
+print(x < 89) # True
+
+# boolean operators
+name = 'Jay'
+age = 19
+if name == 'Jay' and age == 19:
+    print('%s is %d' % (name, age))
+if name == 'James' or name == 'Jay':
+    print('Your name is Jay or James')
