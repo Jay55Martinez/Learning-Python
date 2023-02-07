0eebb1a87b5f5027fd3f64a587269ecc844e72d9
Jay55Martinez <martinez.jay@northeastern.edu>
2023-02-07 17:40:25 -0500

    Module Person Example

--- ModulesAndPackages.py
+++ ModulesAndPackages.py
@@ -1,1 +1,21 @@
-# Modules
+# Modules example import module.py
+from module import Person
+
+# using the imported module to define three people
+mom = Person('Mom', 59, 'she/her', None)
+dad = Person('Dad', 59, 'he/him', None)
+jay = Person('Jay', 19, 'he/him', [mom, dad])
+
+# main function 
+def main():
+    # we can now use methods defined inside the Person class
+    jay.print_reletives()
+    mom.add_relative(jay)
+    dad.add_relative(jay)
+    mom.print_reletives()
+
+# What is the "__name__"? It is a special variable which gets its value depending on how 
+# we execute the containing script.
+# What is "__main__"? is the name of the environment where top-level code is run
+if __name__ == "__main__":
+    main()
--- module.py
+++ module.py
@@ -1,15 +1,38 @@
 # This is a test module that we will import into ModulesAndPackages.py to demonstrate how to use
-# and creat modules
+# and create modules
 class Person:
-    """Class represents a person with a given name, age, pronouns and a list of People that
-    are their reletives"""
-    def __init__(self, name: str, age: int, pronouns: str, relatives=None):
+    """Class represents a person
+    . . .
+    Attributes
+    ----------
+    name : str
+        the name of the person
+    age : int
+        the age of the person
+    pronouns : str
+        the pronouns the person identifies with
+    relatives : list of Person
+        list of all relatives to the person
+    . . .
+    Methods
+    -------
+    add_relatives(other)
+        adds a person to list of relatives. Throws an error if passed a object
+        that is not a Person
+    print_relatives()
+        prints the names of all the relatives for the Person
+    
+    """
+    def __init__(self, name: str, age: int, pronouns: str, relatives: list):
         self.name = name
         self.age = age
         self.pronouns = pronouns
         if relatives is None:
             self.relatives = []
         else:
+            for people in relatives:
+                if not (isinstance(people, Person)):
+                    raise TypeError('The list of relatives contains a object that is not a Person')
             self.relatives = relatives
     
     #  The purpose of __repr__ is to provide a representation of the object that, when passed to the eval
@@ -18,9 +41,12 @@ class Person:
         return f"Person(name={self.name}, age={self.age}, pronouns={self.pronouns}, relatives={[r.name for r in self.relatives]})"
     
     # adds a relative to the list of relatives
-    def add_ralative(self, person):
+    def add_relative(self, person):
+        if not (isinstance(person, Person)):
+            raise TypeError("not given a Person as a parameter")
         self.relatives.append(person)
 
     # prints all the names of the reletives. if list is empty prints 'no reletives'    
     def print_reletives(self):
-        return [relative.name for relative in self.relatives]
+        print("%s's relatives are: " % self.name, end="")
+        print([relative.name for relative in self.relatives])
